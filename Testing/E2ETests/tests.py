94a801bdfeb10a413d27d93edb229568db0db800
Liam <helm_of_protection@hotmail.com>
2023-10-08 21:49:29 -0800

    Completed compression E2E test

--- Testing/E2ETests/tests.py
+++ Testing/E2ETests/tests.py
@@ -16,37 +16,43 @@ def test_compilation():
     # Specify the output path for the compiled executable in the script directory
     output_executable_path = os.path.join(script_directory, "main")
 
-    # Compile command with absolute paths for source files and output
+    # Create the compile command and run it
     compile_command = f"g++ {main_cpp_path} {bitutils_cpp_path} -o {output_executable_path}"
     compile_process = subprocess.Popen(compile_command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
-    # Check for compilation errors
+    # Check results of compilation
     _, compile_error = compile_process.communicate()
 
+
+    # Modify how file is checked based on Linux or Windows
     if platform.system() == "Linux":
         assert os.path.exists("main"), f'Expected main.exe to complile but it does not exist {compile_error}'
     elif platform.system() == "Windows":
         assert os.path.exists(output_executable_path + ".exe"), f'Expected main.exe to complile but it does not exist {compile_error}'
 
-def test_file_creation():
+def test_file_compression():
+
+    # Get absolute path
     path = os.path.dirname(os.path.abspath(__file__))
-    command = f"{path}/main test.txt"
-    compile_process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    _, compile_error1 = compile_process.communicate()
+
+    # Create commands and run commands, collect results
 
     command = f"{path}/main alice_in_wonderland.txt"
-    compile_process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    _, compile_error2 = compile_process.communicate()
+    compression_process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    _, compression_error1 = compression_process.communicate()
 
     command = f"{path}/main kjv.txt"
-    compile_process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    _, compile_error3 = compile_process.communicate()
+    compression_process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    _, compression_error2 = compression_process.communicate()
 
+    # Confirm files are created based on OS
     if platform.system() == "Linux":
-        assert os.path.exists("test.hcmp"), f'Expected file to be created but it was not {compile_error1}'
-        assert os.path.exists("alice_in_wonderland.hcmp"), f'Expected file to be created but it was not {compile_error2}'
-        assert os.path.exists("kjv.hcmp"), f'Expected file to be created but it was not {compile_error3}'
+        assert os.path.exists("alice_in_wonderland.hcmp"), f'Expected file to be created but it was not {compression_error1}'
+        assert os.path.exists("kjv.hcmp"), f'Expected file to be created but it was not {compression_error2}'
     elif platform.system() == "Windows":
-        assert os.path.exists(path + "/test.hcmp"), f'Expected file to be created but it was not {compile_error1}'
-        assert os.path.exists(path + "/alice_in_wonderland.hcmp"), f'Expected file to be created but it was not {compile_error2}'
-        assert os.path.exists(path + "/kjv.hcmp"), f'Expected file to be created but it was not {compile_error3}'
+        assert os.path.exists(path + "/alice_in_wonderland.hcmp"), f'Expected file to be created but it was not {compression_error1}'
+        assert os.path.exists(path + "/kjv.hcmp"), f'Expected file to be created but it was not {compression_error2}'
+
+    # Check that compressed files are smaller than originals
+    assert os.path.getsize(path + "/alice_in_wonderland.hcmp") < os.path.getsize(path + "/alice_in_wonderland.txt")
+    assert os.path.getsize(path + "/kjv.hcmp") < os.path.getsize(path + "/kjv.txt")
